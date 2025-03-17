49b243993e52b59f359dba7afce26bd9dc811081
Nguyen Hai Hoang <haihoang15122002@gmail.com>
2025-03-17 16:23:12 +0700

    Update 17/3/2025 4:23PM

--- demo/footer.php
+++ demo/footer.php
@@ -1,6 +1,6 @@
 <!-- bên dưới -->
 <footer class="bg-dark text-white text-center py-3">
-    <p>&copy; <?php echo date("Y"); ?> My Website. All Rights Reserved.</p>
+    <p>&copy; <?php echo date("Y"); ?> Shop Bed. All Rights Reserved.</p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
